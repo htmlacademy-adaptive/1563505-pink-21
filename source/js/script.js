bef18f72f64f41cdd7cff9ef79207d4a8eb89429
Vladimirshubyn <vladimirshubyn@gmail.com>
2020-12-17 14:20:40 +0700

    JS элементы Pink

---
+++ source/js/script.js
@@ -0,0 +1,4 @@
+const navmain = document.querySelector(".main-nav");
+const navtoggle = document.querySelector(".main-nav__toggle");
+const pageheader = document.querySelector(".page__header");
+const pagepromo = document.querySelector(".promo");
