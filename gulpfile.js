d2d164fa9cee76127738a8d59c4b56f0bb4657c5
Vladimirshubyn <vladimirshubyn@gmail.com>
2020-12-18 14:02:21 +0700

    черновик автоматизации

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
 const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
+const plumber = require("gulp-plumber");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const sourcemap = require("gulp-sourcemaps");
 const sync = require("browser-sync").create();
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+          "source/fonts/**/*.{woff,woff2}",
+          "source/img/**",
+          "source/js/**",
+          "source/*.html"
+     ], {
+       base: "."
+     })
+     .pipe(gulp.dest("build"));
+});
 
 // Styles
 
@@ -22,6 +38,36 @@ const styles = () => {
 }
 
 exports.styles = styles;
+
+// Image min
+
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true})
+      ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+// Sprite
+
+gulp.task("symbols", function() {
+  return gulp.src("build/img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+     }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+// HTML copy
+
+gulp.task("html:copy", function() {
+   return gulp.src("*.html")
+     .pipe(gulp.dest("build"));
+});
 
 // Server
 
@@ -49,3 +95,15 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+gulp.task ("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "js",
+    "images",
+    "symbols",
+    fn
+  );
+});
